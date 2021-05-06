3002662562f9e6147701257f02cce8982f6126c8
Fabio Mendes <60755222+fabioTowers@users.noreply.github.com>
2021-05-06 13:56:24 -0300

    Aula 252

---
+++ revisao_js_array_17/reduce_2/reduce2.js
@@ -0,0 +1,17 @@
+/**Um array de alunos, cada 
+ * elemento é um objeto
+ */
+ const alunos = [
+    { nome: 'João', nota: 7.3, bolsista: false },
+    { nome: 'Maria', nota: 9.2, bolsista: true },
+    { nome: 'Pedro', nota: 9.8, bolsista: false },
+    { nome: 'Ana', nota: 8.7, bolsista: true }
+]
+
+// Desafio 1: Usar reduce para no final dizer se todos alunos são bolsistas ou não
+const todosBolsistas = (resultado, bolsista) => resultado && bolsista
+console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas))
+
+// Desafio 2: Algum aluno é bolsista?
+const algumBolsista = (resultado, bolsista) => resultado || bolsista
+console.log(alunos.map(a => a.bolsista).reduce(algumBolsista))
